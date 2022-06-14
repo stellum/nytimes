c0f8a26388d3a41f048358bf195b33e4be3f2112
oaat9309 <kite1993@naver.com>
2022-06-14 21:30:32 +0900

    feat: Edited SearchInput.jsx (06.14) input 부분입니다.

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 function SearchInput() {
+  const [keyword, setKeyword] = useState("");
+  const [news, setNews] = useState("");
+
+  useEffect(() => {
+    const getArticle = async () => {
+      const api = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=gWFMG5lVIXnJzkE3sellELj5mKHcYlgM`)
+      const data = await api.json()
+      await setNews(data.response.docs);
+    };
+    if (keyword) {
+      const searchTimeout = setTimeout(getArticle, 500);
+      return () => {
+        console.log("clean-up");
+        clearTimeout(searchTimeout);
+      };
+    }
+  }, [keyword]);
+
+  const handleInputChange = (e) => {
+    setKeyword(e.target.value);
+  };
   return (
-    <div>SearchInput</div>
-  )
+    <div>
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요."
+        onChange={handleInputChange}
+      ></input>
+    </div>
+  );
 }
 
-export default SearchInput
+export default SearchInput;
