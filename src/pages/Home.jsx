cee33f392a2239613965de67d3bf154e08694700
sacultang <sacultang@gmail.com>
2022-06-15 19:42:41 +0900

    refactor : Home component API_KEY 분리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,12 @@ import SearchInput from '../components/SearchInput';
 function Home() {
   const [keyword, setKeyword] = useState('');
   const [news, setNews] = useState([]);
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
     const getArticle = async () => {
       const api = await fetch(
-        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=gWFMG5lVIXnJzkE3sellELj5mKHcYlgM`
+        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=${API_KEY}?sort=newest`
       );
       const data = await api.json();
       setNews(data.response.docs);
