81d5f68fb3125c018d6ba8642ddcd1d7102de2d9
sacultang <sacultang@gmail.com>
2022-06-15 17:48:22 +0900

    refactor : News clipCheck 분리

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,20 +1,22 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 function SearchInput() {
-  const [keyword, setKeyword] = useState("");
-  const [news, setNews] = useState("");
+  const [keyword, setKeyword] = useState('');
+  const [news, setNews] = useState('');
 
   useEffect(() => {
     const getArticle = async () => {
-      const api = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=gWFMG5lVIXnJzkE3sellELj5mKHcYlgM`)
-      const data = await api.json()
+      const api = await fetch(
+        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=gWFMG5lVIXnJzkE3sellELj5mKHcYlgM`
+      );
+      const data = await api.json();
       await setNews(data.response.docs);
     };
     if (keyword) {
       const searchTimeout = setTimeout(getArticle, 500);
       return () => {
-        console.log("clean-up");
+        console.log('clean-up');
         clearTimeout(searchTimeout);
       };
     }
@@ -26,8 +28,8 @@ function SearchInput() {
   return (
     <div>
       <input
-        type="text"
-        placeholder="검색어를 입력하세요."
+        type='text'
+        placeholder='검색어를 입력하세요.'
         onChange={handleInputChange}
       ></input>
     </div>
