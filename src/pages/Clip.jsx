05c8bfbfd82066f9caa0fab2cd7ddf0ea10b5308
CHAYUNA <sarasata3131@gmail.com>
2022-06-16 20:44:41 +0900

    error: clip 부분 작동안됨.... 이게 코드가 맞을까요? 클립리스트를 하나 생성했고 클립에서 인자로 받아서 clipped를 보여주고싶은데 렌더링이 안됩니다.코드가 이게 맞을까요?

--- src/pages/Clip.jsx
+++ src/pages/Clip.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ClipList from '../components/ClipList';
 
 function Clip() {
-  return <div>Clip</div>;
+  const clipped = useSelector((state) => state.addClip.clip);
+  console.log(clipped);
+  return (
+    <>
+      <h2>cliplist</h2>
+      <ClipList clip={clipped}/>
+    </>
+  );
 }
 
 export default Clip;
