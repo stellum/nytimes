08238a6149d7b5a770323e33162ac7c938eb2212
Seong1-Jo <seong12yada@gmail.com>
2022-06-17 15:45:17 +0900

    test: Do Header.jsx Test
    다시 test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,23 +5,17 @@ const Container = styled.div`
 color: #8f2b2b;
 `; //임의로 styled Compontent 어떻게 할지 상의
 
-
 function Header() {
-
  let location  = useLocation(); //현재 페이지 위치(url)가져오기
-
  const button = (<button>
  {location.pathname === "/Clip"? "홈으로" : "Clip" }
 </button>);
 
-
   return (
-
     <Container>
       <h2>New York Times</h2>
       {location.pathname === "/Clip"?<Link to="/" rel="noopener noreferrer">{button}</Link> : <Link to="/Clip">{button}</Link> }
     {/* rel 열기 취약점 보안 */}
-
     </Container>
   )
 }
