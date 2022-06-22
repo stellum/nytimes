e32e57109891a0c8b68eaa4c9190c4291f1a976f
CHAYUNA <sarasata3131@gmail.com>
2022-06-22 13:50:24 +0900

    feat : fetchmore 추가

--- src/components/FetchMore.jsx
+++ src/components/FetchMore.jsx
@@ -33,7 +33,7 @@ const FetchMore = ({ loading, setPage }) => {
   });
   useEffect(() => {
     fetchMoreObserver.observe(fetchMoreTrigger.current);
-  }, []);
+  }, [loading]);
   return (
     <>
       <div id="fetchMore" ref={fetchMoreTrigger}>
