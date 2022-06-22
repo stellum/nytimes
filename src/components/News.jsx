4e921da296a614f93e6a31e149f88c7e196ed70a
CHAYUNA <sarasata3131@gmail.com>
2022-06-22 17:54:27 +0900

    refactor: img alt 추가.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -161,6 +161,7 @@ function News(props) {
               ? `https://www.nytimes.com/${props.multimedia}`
               : "https://upload.wikimedia.org/wikipedia/commons/4/40/New_York_Times_logo_variation.jpg"
           }
+          alt={props.headline}
         />
       </Image>
     </Card>
